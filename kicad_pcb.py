c25b425b25807a301a1c06f14f478c059c1a8a8b
krz-max <a1314kevin@gmail.com>
2025-03-27 08:17:44 +0800

    Extend to load net_class

--- kicad_pcb.py
+++ kicad_pcb.py
@@ -7,6 +7,7 @@ it is to implement a parser in an almost declarative way.
 
 A usage demonstration is available in `test.py`
 '''
+import json
 
 try:
     from .sexp_parser import *
@@ -54,6 +55,7 @@ class KicadPCB(SexpParser):
     _module = ['fp_text',
                'fp_circle',
                'fp_arc',
+               'fp_poly',
                'pad',
                'model']
 
@@ -71,12 +73,13 @@ class KicadPCB(SexpParser):
                 'arc',
                 'via',
                 'module',
+                'footprint',
                 ['module'] + _module,
                 ['footprint'] + _module,
                 ('zone',
                     'filled_polygon'))
 
-    _alias_keys = {'footprint' : 'module'}
+    # _alias_keys = {'footprint' : 'module'}
     _parse_module = KicadPCB_module
     _parse_footprint = KicadPCB_module
     _parse_gr_text = KicadPCB_gr_text
@@ -87,8 +90,54 @@ class KicadPCB(SexpParser):
     def getError(self):
         return getSexpError(self)
 
+    # @staticmethod
+    # def load(filename, quote_no_parse=None):
+    #     with open(filename,'r') as f:
+    #         return KicadPCB(parseSexp(f.read(), quote_no_parse))
+    
     @staticmethod
-    def load(filename, quote_no_parse=None, encoding='utf-8'):
-        with open(filename,'r', encoding=encoding) as f:
-            return KicadPCB(parseSexp(f.read(), quote_no_parse))
+    def load(filename):
+        ret = None
+        with open(filename,'r', encoding='utf-8') as f:               # v5 or v6 
+            ret = KicadPCB(parseSexp(f.read()))
+            version = ret.version
+        if (version) > 20211000:  # if v6, extract net_class from project file. 
+            circuitname = filename[0:len(filename)-10]   # assumes project file and pcb file have same name & in same dir
+            net_classes = None
+            try:
+                with open('../benchmarks/'+circuitname+'.kicad_pro') as f:
+                    net_settings = json.load(f)['net_settings']
+                    net_classes = get_netclass(net_settings, "v7")
+                    
+            except FileNotFoundError:
+                print(f"Warrning: kicad_pcb.py detected a kicad v6 file format, but\
+                    \n could not find {circuitname}.kicad_pro in the directory of {circuitname}.kicad_pcb file\
+                    Returning KicadPCB without getting .net_class attribute from project file")
 
+            return ret, net_classes
+        return ret, get_netclass(ret["net_class"], "v5")
+
+def get_netclass(classes, kicad_version="v5"):
+    net_classes = {}
+    if kicad_version == "v5":
+        for net_class in classes:
+            nc_dict = {}
+            nc_dict['width'] = net_class.trace_width
+            nc_dict['clearance'] = net_class.clearance
+            nc_dict['via_diameter'] = net_class.via_dia
+            nc_dict['net_names'] = list()
+            for i in range(len(net_class.add_net)):
+                nc_dict["net_names"].append(net_class.add_net[i])
+            net_classes[net_class[0]] = nc_dict
+    elif kicad_version == "v7":
+        for net_class in classes["classes"]:
+            nc_dict = {}
+            nc_dict['width'] = net_class["track_width"]
+            nc_dict['clearance'] = net_class["clearance"]
+            nc_dict['via_diameter'] = net_class["via_diameter"]
+            nc_dict['net_names'] = list()
+            net_classes[net_class["name"]] = nc_dict
+        if "netclass_patterns" in classes:
+            for pattern in classes["netclass_patterns"]:
+                net_classes[pattern["netclass"]]["net_names"].append(f'"{pattern["pattern"]}"')
+    return net_classes
