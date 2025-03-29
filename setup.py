283396822b794f46d184c9229cf5e736534d0055
krz-max <a1314kevin@gmail.com>
2025-03-29 15:08:13 +0800

    Make this package installable using pip install -e .

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kicad_parser',
+    version='0.1',
+    author='Zheng, Lei',
+    author_email='realthunder.dev@gmail.com',
+    packages=[
+        'kicad_parser',
+        'kicad_parser.sexp_parser',
+    ]
+)
